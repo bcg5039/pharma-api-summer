7661ccc17100bcb854ded67041c7b63f0f4ffd85
bcg5039 <bcg5039@gmail.com>
2017-07-14 13:31:25 -0600

    setup dal

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5555
+
+const dal = require('./dal.js')
+
+//list the meds using dal.listMeds(limit,cb function)
+
+app.get('/', function(req, res, next) {
+  res
+    .status(200)
+    .send('Welcome to the Pharma API! Try a call to GET /meds for starters.')
+})
+
+app.get('/meds', function(req, res, next) {
+  dal.listMeds(10, function(err, data) {
+    if (err) console.log(err)
+    res.status(200).send(data)
+  })
+})
+
+app.listen(port, () => console.log('API up on port:', port))
